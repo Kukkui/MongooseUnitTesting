e2e15874e421f3f4a146bf9e0162d0d38f36c2ae
Jared Hensley <jahensle85@me.com>
2018-04-18 02:09:37 -0500

    comments

--- src/comment.js
+++ src/comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
     content: String,
     user: {
-        // sets a reference tp userid
+        // sets a reference tp userid// 
         type: Schema.Types.ObjectId,
         ref: 'user'
     }
